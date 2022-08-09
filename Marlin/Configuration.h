5434a8deccc828d5155d0f248255f75b8c36896a
stalex78 <80585721+stalex78@users.noreply.github.com>
2022-08-09 19:43:56 +0300

    Fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1194,7 +1194,7 @@
  */
 #define DEFAULT_AXIS_STEPS_PER_UNIT \
   {                                 \
-    228, 227, 8450, 3200, 1000         \
+    228, 227, 8450, 3200, 600       \
   }
 
 /**
@@ -1204,7 +1204,7 @@
  */
 #define DEFAULT_MAX_FEEDRATE \
   {                          \
-    10000, 10000, 300, 5000, 500       \
+    10000, 10000, 300, 5000, 3       \
   }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
@@ -1223,7 +1223,7 @@
  */
 #define DEFAULT_MAX_ACCELERATION \
   {                              \
-    300, 300, 100, 1000, 1000  \
+    300, 300, 100, 1000, 3000  \
   }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
